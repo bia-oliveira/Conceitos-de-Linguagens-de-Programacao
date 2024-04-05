85cf9b362c0348e55a7d68a6b1301e4767df190e
Beatriz Oliveira <164064233+bia-oliveira@users.noreply.github.com>
2024-04-05 00:55:37 -0300

    Add files via upload

---
+++ Proj2a/alexo_binarios.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_SIZE 100
+
+int main() {
+    char input[MAX_SIZE];
+    int cont;
+
+    FILE *file = fopen("input_binarios.txt", "r"); // Abrir o arquivo para leitura
+    if (file == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return 1;
+    }
+
+Q0:
+    if (fgets(input, sizeof(input), file) != NULL) { // le a linha e confere se chegou ao final do arquivo
+        cont = 0;
+        goto Q1;
+    } else {
+        fclose(file); 
+        return 0;
+    }
+
+Q1:
+    if (input[cont] == '0' || input[cont] == '1') { // consome o simbolo 0 ou 1 e passa para o estado Q2
+        cont++;
+        goto Q2;
+    } else {
+        input[strlen(input) - 1] = '\0'; // Remover o caractere de nova linha
+        printf("%s      Cadeia rejeitada.\n", input); // se comecar com outra coisa, a cadeia e rejeitada
+        goto Q0;
+    }
+
+Q2:
+    if (input[cont] == '\0' || input[cont] == '\n') { // confere se chegou ao final da cadeia, se sim, ela é aceita
+        input[cont] = '\0'; // Substituir o caractere de nova linha por um caractere nulo
+        printf("%s      Cadeia aceita.\n", input);
+        goto Q0;
+    } else if (input[cont] == '0' || input[cont] == '1') { // consome os caracteres restantes ate chegar ao final da cadeia
+        cont++;
+        goto Q2;
+    } else {
+        input[strlen(input) - 1] = '\0'; // Remover o caractere de nova linha
+        printf("%s      Cadeia rejeitada.\n", input);
+        goto Q0;
+    }
+}
---
+++ Proj2a/alexo_decimais.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_SIZE 100
+
+int main() {
+    char input[MAX_SIZE];
+    int cont;
+
+    FILE *file = fopen("input_decimais.txt", "r"); // Abrir o arquivo para leitura
+    if (file == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return 1;
+    }
+
+Q0:
+    if (fgets(input, sizeof(input), file) != NULL) {
+        cont = 0;
+        goto Q1;
+    } else {
+        fclose(file);
+        return 0;
+    }
+
+Q1:
+    if (input[cont] == '+' || input[cont] == '-') { // "consome" o primeiro simbolo da cadeia, que e o sinal
+        cont++;
+        goto Q2;
+    } else {
+        input[strlen(input) - 1] = '\0'; // Remover o caractere de nova linha
+        printf("%s      Cadeia rejeitada.\n", input); // rejeita se comecar com outro simbolo
+        goto Q0;
+    }
+
+Q2: 
+    if(input[cont] >= '0' && input[cont] <= '9'){ // "consome" os simbolos da cadeia antecessores ao ".", que sao digitos
+        cont++;
+        goto Q2;
+    } else if(input[cont] == '.'){ // "consome" o simbolo "."
+        cont++;
+        goto Q3;  
+    } else{
+        input[strlen(input) - 1] = '\0'; // Remover o caractere de nova linha
+        printf("%s      Cadeia rejeitada.\n", input);
+        goto Q0;
+    }
+
+Q3:
+    if (input[cont] == '\0' || input[cont] == '\n') { // "consome" os simbolos da cadeia sucessores ao ".", que sao digitos
+        input[cont] = '\0'; // Substituir o caractere de nova linha por um caractere nulo
+        printf("%s      Cadeia aceita.\n", input);
+        goto Q0;
+    } else if (input[cont] >= '0' && input[cont] <= '9') { // se foi possivel chegar ao final da cadeia, entao a palavra e aceita
+        cont++;
+        goto Q3;
+    } else {
+        input[strlen(input) - 1] = '\0'; // Remover o caractere de nova linha
+        printf("%s      Cadeia rejeitada.\n", input);
+        goto Q0;
+    }
+  
+}
