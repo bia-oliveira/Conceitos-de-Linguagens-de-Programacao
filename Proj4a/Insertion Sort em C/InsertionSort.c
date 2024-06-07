1f7d2afdde4384bb937d5f5db17b57fe9c75cede
Beatriz Oliveira <164064233+bia-oliveira@users.noreply.github.com>
2024-06-06 21:42:54 -0300

    Update and rename Proj4a/Insertion Sort C to Proj4a/Insertion Sort em C/InsertionSort.c

---
+++ Proj4a/Insertion Sort em C/InsertionSort.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void generateRandomArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % 1000; 
+    }
+}
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+
+    srand(time(0)); // Inicializa o gerador de números aleatórios
+
+    int arr10[10];
+
+    generateRandomArray(arr10, 10);
+  
+    int n = sizeof(arr10) / sizeof(arr10[0]);
+    insertionSort(arr10, n);
+    printArray(arr10, n);
+    return 0;
+}
