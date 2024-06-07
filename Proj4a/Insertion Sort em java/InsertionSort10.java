588f63a2bf0bef31b5513a3797a17365699632f2
Beatriz Oliveira <164064233+bia-oliveira@users.noreply.github.com>
2024-06-06 22:06:28 -0300

    Create InsertionSort10.java

---
+++ Proj4a/Insertion Sort em java/InsertionSort10.java
@@ -0,0 +1,41 @@
+import java.util.Random;
+
+public class InsertionSort {
+
+    public static void generateRandomArray(int[] arr) {
+        Random rand = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt(1000); 
+        }
+    }
+    
+    void insertionSort(int arr[]) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        InsertionSort ob = new InsertionSort();
+        
+        int[] arr10 = new int[10];
+
+        generateRandomArray(arr10);
+        ob.insertionSort(arr10);
+        ob.printArray(arr10);
+    }
+}
